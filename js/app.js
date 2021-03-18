2be72158d9cc3dfa504e6ca4dd6fa67b3166f150
jgutierrezCSU <jgutierrez@mail.csuchico.edu>
2021-03-18 15:17:06 -0700

    Moving edit profile to its own page

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
-// xxxxxxxxxx Working For Sign Up Form xxxxxxxxxx
-// xxxxxxxxxx Full Name Validation xxxxxxxxxx
+// For Sign Up Form and Full Name Validation 
 function checkUserFullName() {
     var userLastname = document.getElementById("userFullName").value;
+    console.log(userLastname)
     var flag = false;
     if (userLastname === "") {
         flag = true;
     }
+    console.log(flag)
     if (flag) {
         document.getElementById("userFullNameError").style.display = "block";
     } else {
         document.getElementById("userFullNameError").style.display = "none";
     }
 }
-// xxxxxxxxxx User Lastname Validation xxxxxxxxxx
+//  Lastname Validation 
 function checkUserLastname() {
     var userLastname = document.getElementById("userLastname").value;
     var flag = false;
@@ -25,7 +26,7 @@ function checkUserLastname() {
         document.getElementById("userLastnameError").style.display = "none";
     }
 }
-// xxxxxxxxxx Email Validation xxxxxxxxxx
+//  Email Validation
 function checkUserEmail() {
     var userEmail = document.getElementById("userEmail");
     var userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -41,7 +42,7 @@ function checkUserEmail() {
         document.getElementById("userEmailError").style.display = "none";
     }
 }
-// xxxxxxxxxx Password Validation xxxxxxxxxx
+// Password Validation 
 function checkUserPassword() {
     var userPassword = document.getElementById("userPassword");
     var userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;
@@ -57,7 +58,7 @@ function checkUserPassword() {
         document.getElementById("userPasswordError").style.display = "none";
     }
 }
-// xxxxxxxxxx Check user bio characters. It'll use later xxxxxxxxxx
+// Check user bio characters. It'll use later 
 function checkUserBio() {
     var userBio = document.getElementById("userBio").value;
     var flag = false;
@@ -67,7 +68,7 @@ function checkUserBio() {
         document.getElementById("userBioError").style.display = "none";
     }
 }
-// xxxxxxxxxx Submitting and Creating new user in firebase authentication xxxxxxxxxx
+//  Submitting and Creating new user in firebase authentication 
 function signUp() {
     var userFullName = document.getElementById("userFullName").value;
     var userLastname = document.getElementById("userLastname").value;
@@ -90,8 +91,7 @@ function signUp() {
     } else if (checkUserPasswordValid == null) {
         return checkUserPassword();
     } else {
-        console.log(userEmail)
-        console.log(userPassword)
+
         firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).then((success) => {
 
             var user = firebase.auth().currentUser;
@@ -101,7 +101,7 @@ function signUp() {
             if (user != null) {
                 uid = user.uid;
             }
-            console.log(uid)
+
             var firebaseRef = firebase.database().ref();
             var userData = {
                 userFullName: userFullName,
@@ -112,6 +112,7 @@ function signUp() {
                 userTw: "https://twitter.com/",
                 userGp: "https://plus.google.com/",
                 userBio: "User biography",
+                userTookSurvey: "False",
             }
             firebaseRef.child(uid).set(userData);
             swal('Your Account Created', 'Your account was created successfully, you can log in now.',
@@ -132,8 +133,7 @@ function signUp() {
         });
     }
 }
-// xxxxxxxxxx Working For Sign In Form xxxxxxxxxx
-// xxxxxxxxxx Sign In Email Validation xxxxxxxxxx
+//  For Sign In / Index Form  Sign In Email Validation 
 function checkUserSIEmail() {
     var userSIEmail = document.getElementById("userSIEmail");
     var userSIEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -149,7 +149,7 @@ function checkUserSIEmail() {
         document.getElementById("userSIEmailError").style.display = "none";
     }
 }
-// xxxxxxxxxx Sign In Password Validation xxxxxxxxxx
+//  Sign In Password Validation 
 function checkUserSIPassword() {
     var userSIPassword = document.getElementById("userSIPassword");
     var userSIPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;
@@ -165,7 +165,7 @@ function checkUserSIPassword() {
         document.getElementById("userSIPasswordError").style.display = "none";
     }
 }
-// xxxxxxxxxx Check email or password exsist in firebase authentication xxxxxxxxxx    
+//  Check email or password exsist in firebase authentication     
 function signIn() {
     var userSIEmail = document.getElementById("userSIEmail").value;
     var userSIPassword = document.getElementById("userSIPassword").value;
@@ -181,7 +181,10 @@ function signIn() {
         return checkUserSIPassword();
     } else {
         firebase.auth().signInWithEmailAndPassword(userSIEmail, userSIPassword).then((success) => {
+
+
             window.location.replace("./pages/profile.html");
+
 
         }).catch((error) => {
             // Handle Errors here.
@@ -193,11 +196,14 @@ function signIn() {
                 text: "Error",
             })
         });
+
+
     }
 }
-// xxxxxxxxxx Working For Profile Page xxxxxxxxxx
-// xxxxxxxxxx Get data from server and show in the page xxxxxxxxxx
-firebase.auth().onAuthStateChanged((user) => {
+
+
+function updateProfile(user) {
+    console.log("in updateProfile")
     if (user) {
         //   User is signed in.
         let user = firebase.auth().currentUser;
@@ -207,24 +213,59 @@ firebase.auth().onAuthStateChanged((user) => {
         }
         let firebaseRefKey = firebase.database().ref().child(uid);
         firebaseRefKey.on('value', (dataSnapShot) => {
-            console.log(dataSnapShot.val().userFullName)
-            console.log(dataSnapShot.val().userLastname)
+
             document.getElementById("userPfFullName").innerHTML = dataSnapShot.val().userFullName;
             document.getElementById("userPfLastname").innerHTML = dataSnapShot.val().userLastname;
-            userEmail = dataSnapShot.val().userEmail;
-            userPassword = dataSnapShot.val().userPassword;
-            // document.getElementById("userPfFb").setAttribute('href', dataSnapShot.val().userFb);
-            // document.getElementById("userPfTw").setAttribute('href', dataSnapShot.val().userTw);
-            // document.getElementById("userPfGp").setAttribute('href', dataSnapShot.val().userGp);
-            // document.getElementById("userPfBio").innerHTML = dataSnapShot.val().userBio;
+            // userEmail = dataSnapShot.val().userEmail;
+            //userPassword = dataSnapShot.val().userPassword;
+
+            document.getElementById("userPfFb").setAttribute('href', dataSnapShot.val().userFb);
+            document.getElementById("userPfTw").setAttribute('href', dataSnapShot.val().userTw);
+            document.getElementById("userPfGp").setAttribute('href', dataSnapShot.val().userGp);
+            document.getElementById("userPfBio").innerHTML = dataSnapShot.val().userBio;
         })
     } else {
         //   No user is signed in.
     }
+
+
+
+}
+
+// For Profile Page Get data from server and display
+//listener for log in / log out status
+firebase.auth().onAuthStateChanged((user) => {
+    console.log("onAuth")
+    updateProfile(user);
+    // if (user) {
+    //     //   User is signed in.
+    //     let user = firebase.auth().currentUser;
+    //     let uid
+    //     if (user != null) {
+    //         uid = user.uid;
+    //     }
+    //     let firebaseRefKey = firebase.database().ref().child(uid);
+    //     firebaseRefKey.on('value', (dataSnapShot) => {
+
+    //         document.getElementById("userPfFullName").innerHTML = dataSnapShot.val().userFullName;
+    //         document.getElementById("userPfLastname").innerHTML = dataSnapShot.val().userLastname;
+    //         // userEmail = dataSnapShot.val().userEmail;
+    //         //userPassword = dataSnapShot.val().userPassword;
+
+    //         document.getElementById("userPfFb").setAttribute('href', dataSnapShot.val().userFb);
+    //         document.getElementById("userPfTw").setAttribute('href', dataSnapShot.val().userTw);
+    //         document.getElementById("userPfGp").setAttribute('href', dataSnapShot.val().userGp);
+    //         document.getElementById("userPfBio").innerHTML = dataSnapShot.val().userBio;
+    //     })
+    // } else {
+    //     //   No user is signed in.
+    // }
 });
 
-// xxxxxxxxxx Show edit profile form with detail xxxxxxxxxx
+
+//  Show edit profile form with detail 
 function showEditProfileForm() {
+
     document.getElementById("profileSection").style.display = "none"
     document.getElementById("editProfileForm").style.display = "block"
     var userPfFullName = document.getElementById("userPfFullName").innerHTML;
@@ -240,12 +281,12 @@ function showEditProfileForm() {
     document.getElementById("userGooglePlus").value = userPfGp;
     document.getElementById("userBio").value = userPfBio;
 }
-// xxxxxxxxxx Hide edit profile form xxxxxxxxxx
+//  Hide edit profile form 
 function hideEditProfileForm() {
     document.getElementById("profileSection").style.display = "block";
     document.getElementById("editProfileForm").style.display = "none";
 }
-// xxxxxxxxxx Save profile and update database xxxxxxxxxx
+//  Save profile and update database 
 function saveProfile() {
     let userFullName = document.getElementById("userFullName").value
     let userLastname = document.getElementById("userLastname").value
@@ -260,35 +301,39 @@ function saveProfile() {
     } else if (userLastname === "") {
         return checkUserLastname();
     } else {
+
         let user = firebase.auth().currentUser;
         let uid;
         if (user != null) {
             uid = user.uid;
         }
-        var firebaseRef = firebase.database().ref();
-        var userData = {
-            userFullName: userFullName,
-            userLastname: userLastname,
-            userFb: userFacebook,
-            userTw: userTwitter,
-            userGp: userGooglePlus,
-            userBio: userBio,
-        }
-        firebaseRef.child(uid).set(userData);
-        swal({
-            type: 'successfull',
-            title: 'Update successfull',
-            text: 'Profile updated.',
-        }).then((value) => {
-            setTimeout(function () {
-                document.getElementById("profileSection").style.display = "block";
+        let firebaseRefKey = firebase.database().ref().child(uid);
+        firebaseRefKey.on('value', (dataSnapShot) => {
+            var firebaseRef = firebase.database().ref();
+            var userData = {
+                // These parts are the info that user changed/ can change
+                userFullName: userFullName,
+                userLastname: userLastname,
+                userFb: userFacebook,
+                userTw: userTwitter,
+                userGp: userGooglePlus,
+                userBio: userBio,
+                //This part remains the same, get data from DB and input same info (unchaged) 
+                userEmail: dataSnapShot.val().userEmail,
+                userTookSurveySur: "True",
+            }
+            console.log("In Save Profile")
+            firebaseRef.child(uid).set(userData);
+        })
 
-                document.getElementById("editProfileForm").style.display = "none";
-            }, 1000)
-        });
+        // document.getElementById("profileSection").style.display = "block";
+
+        // document.getElementById("editProfileForm").style.display = "none";
+        hideEditProfileForm();
+
     }
 }
-// xxxxxxxxxx Working For Sign Out xxxxxxxxxx
+// Working For Sign Out 
 function signOut() {
     firebase.auth().signOut().then(function () {
         // Sign-out successful.
@@ -310,3 +355,93 @@ function signOut() {
         })
     });
 }
+function showTakeSurvey() {
+
+    // for SurveyJS
+    document.getElementById("profileSection").style.display = "none"
+    document.getElementById("surveyContainer").style.display = "block"
+
+    Survey.StylesManager.applyTheme("modern");
+
+    var surveyJSON = { "pages": [{ "name": "page1", "elements": [{ "type": "ranking", "name": "Rank your Favorite category", "title": "Rank your Favorite category ", "description": "Ranking Categories", "isRequired": true, "choices": ["Horror", "Romance", "Sci-FI", "Action", "Comedy"] }, { "type": "multipletext", "name": "last 3 series you watch", "title": "What was the last 3 series you watch ?", "description": "last 3 series you watch", "items": [{ "name": "Series 1" }, { "name": "Series 2" }, { "name": "Series 3" }] }, { "type": "panel", "name": "panel1", "elements": [{ "type": "html", "name": "question1", "html": "<h1> Are you a fan of any of these Producers ? </h1>" }] }, { "type": "boolean", "name": "Quentin Tarantino", "title": "Quentin Tarantino" }, { "type": "boolean", "name": "Kathleen Kennedy", "title": "Kathleen Kennedy" }, { "type": "boolean", "name": "Spike Lee", "title": "Spike Lee" }, { "type": "boolean", "name": "Kevin Feige", "title": "Kevin Feige" }] }], "showCompletedPage": false }
+
+    function sendDataToServer(survey) {
+        //send Ajax request to your web server.
+        document.getElementById("profileSection").style.display = "block";
+
+        document.getElementById("surveyContainer").style.display = "none";
+
+        alert("The results are:" + JSON.stringify(survey.data));
+
+    }
+
+    var survey = new Survey.Model(surveyJSON);
+    $("#surveyContainer").Survey({
+        async: true,
+        model: survey,
+        onComplete: sendDataToServer
+    });
+
+    document.getElementById("takeSurveybtn").innerHTML = "Retake Survey";
+
+    // set RetakeSurvey value to true
+    setRetakeTestTrue();
+
+}
+
+
+// function checkIfTestTaken() {
+//     if (user) {
+//         //   User is signed in.
+//         let user = firebase.auth().currentUser;
+//         let uid
+//         if (user != null) {
+//             uid = user.uid;
+//         }
+//         let firebaseRefKey = firebase.database().ref().child(uid);
+//         firebaseRefKey.on('value', (dataSnapShot) => {
+//             if (dataSnapShot.val().userEmail == "True") {
+//                 document.getElementById("takeSurveybtn").innerHTML = "Retake Survey";
+//             }
+//         })
+
+//     }
+
+// }
+function setRetakeTestTrue() {
+    console.log("in setRetakeTestTrue")
+    let user = firebase.auth().currentUser;
+    let uid;
+    if (user != null) {
+        uid = user.uid;
+    }
+
+    let firebaseRefKey = firebase.database().ref().child(uid);
+    firebaseRefKey.on('value', (dataSnapShot) => {
+
+        var firebaseRef = firebase.database().ref();
+        //Create object with Data
+        if (dataSnapShot.val().userTookSurveySu == "False") {
+            var userData = {
+                // //get current values from Firebase
+                userFullName: dataSnapShot.val().userFullName,
+                userLastname: dataSnapShot.val().userLastname,
+                userEmail: dataSnapShot.val().userEmail,
+                userFb: dataSnapShot.val().userFb,
+                userTw: dataSnapShot.val().userTw,
+                userGp: dataSnapShot.val().userGp,
+                userBio: dataSnapShot.val().userBio,
+                userTookSurveySur: "True", // this only value we change
+            }
+            //send that object to firebase
+            firebaseRef.child(uid).set(userData);
+        }
+    })
+
+
+
+}
+
+
+
+
